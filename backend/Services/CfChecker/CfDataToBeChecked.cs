4ee0068b47c14f58cdce8f1c803a042003f2e63f
supix <esposito.marce@gmail.com>
2018-05-08 22:11:39 +0200

    CfDataToBeChecked class is immutable.

--- backend/Services/CfChecker/CfDataToBeChecked.cs
+++ backend/Services/CfChecker/CfDataToBeChecked.cs
@@ -7,10 +7,19 @@ namespace Services.CfChecker
 {
     public class CfDataToBeChecked
     {
-        public string FiscalCode { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public DateTime BirthDate { get; set; }
-        public string Pin { get; set; }
+        public CfDataToBeChecked(string fiscalCode, string firstName, string lastName, DateTime birthDate, string pin)
+        {
+            FiscalCode = fiscalCode ?? throw new ArgumentNullException(nameof(fiscalCode));
+            FirstName = firstName ?? throw new ArgumentNullException(nameof(firstName));
+            LastName = lastName ?? throw new ArgumentNullException(nameof(lastName));
+            BirthDate = birthDate;
+            Pin = pin ?? throw new ArgumentNullException(nameof(pin));
+        }
+
+        public string FiscalCode { get; }
+        public string FirstName { get; }
+        public string LastName { get; }
+        public DateTime BirthDate { get; }
+        public string Pin { get; }
     }
 }
