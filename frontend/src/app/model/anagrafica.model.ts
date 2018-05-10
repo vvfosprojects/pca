c1542c61122bd2153e774e08263ecabb3211f7b8
unknown <manuela.marzotti@vigilfuoco.it>
2018-05-10 14:21:30 +0200

    Configurato webservice per il controllo del codice fiscale

---
+++ frontend/src/app/model/anagrafica.model.ts
@@ -0,0 +1,12 @@
+export class Anagrafica {
+
+    constructor(
+        public fiscalCode: string,
+        public firstName: string,
+        public lastName: string,
+        public birthDate: Date,
+        public pin: string
+    ) {}
+
+}
+
