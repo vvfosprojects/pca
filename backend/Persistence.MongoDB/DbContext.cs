d2c863aafee721eefefd871c2f50c271ee9e0f17
supix <esposito.marce@gmail.com>
2018-05-13 15:16:01 +0200

    DbPersistence projects available
    DbPersistence project is available. It implements a persistence layer
    based on MongoDB to save applications. A DbContext class has been
    sketched. It creates an index on FiscalCodes and deletion times (in
    order to efficiently access only active applications).

---
+++ backend/Persistence.MongoDB/DbContext.cs
@@ -0,0 +1,67 @@
+﻿using DomainModel;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Conventions;
+using MongoDB.Bson.Serialization.IdGenerators;
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Driver;
+
+namespace Persistence.MongoDB
+{
+    internal class DbContext
+    {
+        private readonly IMongoDatabase database;
+
+        public DbContext(string mongoUrl)
+        {
+            InitDbSettings();
+            MapClasses();
+            this.database = InitDbInstance(mongoUrl);
+            CreateIndexes();
+        }
+
+        private IMongoDatabase InitDbInstance(string mongoUrl)
+        {
+            var url = new MongoUrl(mongoUrl);
+            var client = new MongoClient(mongoUrl);
+            return client.GetDatabase(url.DatabaseName);
+        }
+
+        private void InitDbSettings()
+        {
+            var pack = new ConventionPack();
+            pack.Add(new CamelCaseElementNameConvention());
+            ConventionRegistry.Register("camel case", pack, t => true);
+        }
+
+        private void CreateIndexes()
+        {
+            {
+                var indexDefinition = Builders<Application>.IndexKeys
+                    .Ascending(_ => _.FiscalCode)
+                    .Ascending(_ => _.DeletionTime);
+
+                this.Applications.Indexes.CreateOne(indexDefinition);
+            }
+        }
+
+        private void MapClasses()
+        {
+            BsonClassMap.RegisterClassMap<Application>(cm =>
+            {
+                cm.AutoMap();
+                cm.MapIdMember(c => c.Id)
+                    .SetIdGenerator(StringObjectIdGenerator.Instance)
+                    .SetSerializer(new StringSerializer(BsonType.ObjectId));
+            });
+        }
+
+        public IMongoCollection<Application> Applications
+        {
+            get
+            {
+                return this.database.GetCollection<Application>("applications");
+            }
+        }
+    }
+}
