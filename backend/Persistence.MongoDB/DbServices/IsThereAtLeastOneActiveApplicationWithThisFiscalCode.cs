d7cf4647d1c0bfc4a73b000be2d8a43cfb6d487e
supix <esposito.marce@gmail.com>
2018-05-13 15:29:27 +0200

    Persistence service implementation
    The service to check whether at least an application is active given its
    fiscal code is available.
    
    A service to store an application is available.

---
+++ backend/Persistence.MongoDB/DbServices/IsThereAtLeastOneActiveApplicationWithThisFiscalCode.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+
+namespace Persistence.MongoDB.DbServices
+{
+    internal class IsThereAtLeastOneActiveApplicationWithThisFiscalCode
+    {
+        private readonly DbContext dbContext;
+
+        public IsThereAtLeastOneActiveApplicationWithThisFiscalCode(DbContext dbContext)
+        {
+            this.dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+        }
+
+        public bool Check(string fiscalCode)
+        {
+            return this.dbContext.Applications.Find(a =>
+                a.FiscalCode == fiscalCode &&
+                a.DeletionTime == null)
+                .Any();
+        }
+    }
+}
