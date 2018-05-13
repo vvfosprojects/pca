234e19f4f48cd60149a6bc9542498c3cc7a77126
supix <esposito.marce@gmail.com>
2018-05-13 16:05:28 +0200

    New services
    ICanUpdateApplication interface checks whether fiscal code and pin match for an active application. Service is implemented in the persistence layer. A new controller exposes this functionality.
    ISubmitApplication is a service allowing to submit a new application.

---
+++ backend/PCA/Controllers/CanUpdateApplicationController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DomainModel.Services;
+
+namespace PCA.Controllers
+{
+    public class CanUpdateApplicationController : ApiController
+    {
+        private readonly ICanUpdateApplication canUpdateApplication;
+
+        public CanUpdateApplicationController(ICanUpdateApplication canUpdateApplication)
+        {
+            this.canUpdateApplication = canUpdateApplication ?? throw new ArgumentNullException(nameof(canUpdateApplication));
+        }
+
+        public bool CanUpdate(string fiscalCode, string pin)
+        {
+            return this.canUpdateApplication.CanUpdate(fiscalCode, pin);
+        }
+    }
+}
---
+++ backend/Persistence.MongoDB/DbServices/IsThereAnActiveApplicationByFiscalCodeAndPin.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DomainModel.Services;
+using MongoDB.Driver;
+
+namespace Persistence.MongoDB.DbServices
+{
+    internal class IsThereAnActiveApplicationByFiscalCodeAndPin : ICanUpdateApplication
+    {
+        private readonly DbContext dbContext;
+
+        public IsThereAnActiveApplicationByFiscalCodeAndPin(DbContext dbContext)
+        {
+            this.dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+        }
+
+        public bool Check(string fiscalCode, string pin)
+        {
+            return this.dbContext.Applications.Find(a =>
+                a.FiscalCode == fiscalCode &&
+                a.DeletionTime == null &&
+                a.GetPin() == pin.ToUpper())
+                .Any();
+        }
+
+        bool ICanUpdateApplication.CanUpdate(string fiscalCode, string pin)
+        {
+            return this.Check(fiscalCode, pin);
+        }
+    }
+}
