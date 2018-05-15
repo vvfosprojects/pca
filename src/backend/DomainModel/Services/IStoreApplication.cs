55267af025a3441d9ff12d8dc1eeca95903a27a6
supix <esposito.marce@gmail.com>
2018-05-15 21:37:10 +0200

    All files have licence headers

--- src/backend/DomainModel/Services/IStoreApplication.cs
+++ src/backend/DomainModel/Services/IStoreApplication.cs
@@ -1,4 +1,23 @@
-﻿namespace DomainModel.Services
+﻿//-----------------------------------------------------------------------
+// <copyright file="IStoreApplication.cs" company="CNVVF">
+// Copyright (C) 2018 - CNVVF
+//
+// This file is part of Public Competition Application (PCA).
+// PCA is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// PCA is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace DomainModel.Services
 {
     public interface IStoreApplication
     {
