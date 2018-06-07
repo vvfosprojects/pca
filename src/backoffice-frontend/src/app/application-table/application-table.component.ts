172d35b171544dfc9e1ba8a18ca5e925d6d14bbd
supix <esposito.marce@gmail.com>
2018-06-07 23:58:32 +0200

    Application table asynchronous loaded
    Table shown as soon as the data are made available by the
    backend, not before. This prevent a null reference exception.

--- src/backoffice-frontend/src/app/application-table/application-table.component.ts
+++ src/backoffice-frontend/src/app/application-table/application-table.component.ts
@@ -17,64 +17,63 @@ export class ApplicationTableComponent implements OnInit {
 
   private curPage: number = 0;
   private pageSize: number = 5;
-  
-  private page: ApplicationRowPage;
-  
-  private lastPage : boolean = false;
-  private firstPage : boolean = true;
-  
- /*  private numberPage : number = 1;
-  private totPage = ['1','2','3'];
- */
-  
+
+  private page: ApplicationRowPage = null;
+
+  private lastPage: boolean = false;
+  private firstPage: boolean = true;
+
+  /*  private numberPage : number = 1;
+   private totPage = ['1','2','3'];
+  */
+
   constructor(private router: Router, private getApplicationRowsService: GetApplicationRowsService) { }
- 
+
   ngOnInit() {
     this.getApplicationRowsService.getRows(this.curPage * this.pageSize, this.pageSize)
-      .subscribe(page => this.page = page);
-      if ((this.curPage+this.pageSize >= this.page.totalCount) ||  (this.page.totalCount == 0))
-      {
-        this.lastPage = true;
-      }  
-      else
-        this.lastPage = false;
+      .subscribe(page => {
+        this.page = page;
+        if ((this.curPage + this.pageSize >= this.page.totalCount) || (this.page.totalCount == 0)) {
+          this.lastPage = true;
+        }
+        else
+          this.lastPage = false;
+      });
   }
 
 
   private onNext() {
-    this.curPage = this.curPage+this.pageSize;
+    this.curPage = this.curPage + this.pageSize;
     // this.numberPage = this.numberPage + 1;
     this.getApplicationRowsService.getRows(this.curPage, this.pageSize)
-    .subscribe(page => this.page = page);
-    if ((this.curPage+this.pageSize >= this.page.totalCount) ||  (this.page.totalCount == 0)) 
-    {
-       this.lastPage = true;
-       this.firstPage = false;
-      }
-    else
+      .subscribe(page => this.page = page);
+    if ((this.curPage + this.pageSize >= this.page.totalCount) || (this.page.totalCount == 0)) {
+      this.lastPage = true;
       this.firstPage = false;
     }
-
-  private onPrev() {
-    this.curPage = this.curPage-this.pageSize;
-    // this.numberPage = this.numberPage - 1;
-    this.getApplicationRowsService.getRows(this.curPage, this.pageSize)
-    .subscribe(page => this.page = page);
-    if (this.curPage <= 0)  
-    {
-       this.firstPage = true;
-       this.lastPage = false;
-    }
     else
-       this.lastPage = false;
+      this.firstPage = false;
   }
 
-   private mostraApplication(row: ApplicationRow) {
-     console.log(row);
-     this.router.navigate(['/application-detail', row.id]);
+  private onPrev() {
+    this.curPage = this.curPage - this.pageSize;
+    // this.numberPage = this.numberPage - 1;
+    this.getApplicationRowsService.getRows(this.curPage, this.pageSize)
+      .subscribe(page => this.page = page);
+    if (this.curPage <= 0) {
+      this.firstPage = true;
+      this.lastPage = false;
     }
+    else
+      this.lastPage = false;
+  }
+
+  private mostraApplication(row: ApplicationRow) {
+    console.log(row);
+    this.router.navigate(['/application-detail', row.id]);
+  }
 
 }
-  
-  
- 
+
+
+
