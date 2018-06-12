fc3f864a092fef01b5077f82daa7a9e2237d6ed8
unknown <manuela.marzotti@vigilfuoco.it>
2018-06-12 10:20:02 +0200

    viewvalue of buGroups is changed

--- src/frontend/src/app/application-form/bu-groups.ts
+++ src/frontend/src/app/application-form/bu-groups.ts
@@ -3,208 +3,208 @@ export const BuGroups = [
         name: 'Piemonte',
         bu: [
             { value: 'DIR-PIE', viewValue: 'Dir. Reg. Piemonte' },
-            { value: 'COM-TO', viewValue: 'Com. Prov. Torino' },
-            { value: 'COM-AL', viewValue: 'Com. Prov. Alessandria' },
-            { value: 'COM-AT', viewValue: 'Com. Prov. Asti' },
-            { value: 'COM-BI', viewValue: 'Com. Prov. Biella' },
-            { value: 'COM-CN', viewValue: 'Com. Prov. Cuneo' },
-            { value: 'COM-NO', viewValue: 'Com. Prov. Novara' },
-            { value: 'COM-VB', viewValue: 'Com. Prov. Verbano-Cusio-Ossola' },
-            { value: 'COM-VE', viewValue: 'Com. Prov. Vercelli' }
+            { value: 'COM-TO', viewValue: 'Com. Torino' },
+            { value: 'COM-AL', viewValue: 'Com. Alessandria' },
+            { value: 'COM-AT', viewValue: 'Com. Asti' },
+            { value: 'COM-BI', viewValue: 'Com. Biella' },
+            { value: 'COM-CN', viewValue: 'Com. Cuneo' },
+            { value: 'COM-NO', viewValue: 'Com. Novara' },
+            { value: 'COM-VB', viewValue: 'Com. Verbano-Cusio-Ossola' },
+            { value: 'COM-VE', viewValue: 'Com. Vercelli' }
       ]
     },
     {
         name: 'Lombardia',
         bu: [
             { value: 'DIR-LOM', viewValue: 'Dir. Reg. Lombardia' },
-            { value: 'COM-MI', viewValue: 'Com. Prov. Milano' },
-            { value: 'COM-BG', viewValue: 'Com. Prov. Bergamo' },
-            { value: 'COM-BS', viewValue: 'Com. Prov. Brescia' },
-            { value: 'COM-CO', viewValue: 'Com. Prov. Como' },
-            { value: 'COM-CR', viewValue: 'Com. Prov. Cremona' },
-            { value: 'COM-LC', viewValue: 'Com. Prov. Lecco' },
-            { value: 'COM-LO', viewValue: 'Com. Prov. Lodi' },
-            { value: 'COM-MN', viewValue: 'Com. Prov. Mantova' },
-            { value: 'COM-PV', viewValue: 'Com. Prov. Pavia' },
-            { value: 'COM-SO', viewValue: 'Com. Prov. Sondrio' },
-            { value: 'COM-VA', viewValue: 'Com. Prov. Varese' }
+            { value: 'COM-MI', viewValue: 'Com. Milano' },
+            { value: 'COM-BG', viewValue: 'Com. Bergamo' },
+            { value: 'COM-BS', viewValue: 'Com. Brescia' },
+            { value: 'COM-CO', viewValue: 'Com. Como' },
+            { value: 'COM-CR', viewValue: 'Com. Cremona' },
+            { value: 'COM-LC', viewValue: 'Com. Lecco' },
+            { value: 'COM-LO', viewValue: 'Com. Lodi' },
+            { value: 'COM-MN', viewValue: 'Com. Mantova' },
+            { value: 'COM-PV', viewValue: 'Com. Pavia' },
+            { value: 'COM-SO', viewValue: 'Com. Sondrio' },
+            { value: 'COM-VA', viewValue: 'Com. Varese' }
       ]
     },
     {
         name: 'Veneto',
         bu: [
             { value: 'DIR-VEN', viewValue: 'Dir. Reg. Veneto' },
-            { value: 'COM-VE', viewValue: 'Com. Prov. Venezia' },
-            { value: 'COM-BL', viewValue: 'Com. Prov. Belluno' },
-            { value: 'COM-PD', viewValue: 'Com. Prov. Padova' },
-            { value: 'COM-RO', viewValue: 'Com. Prov. Rovigo' },
-            { value: 'COM-TV', viewValue: 'Com. Prov. Treviso' },
-            { value: 'COM-VI', viewValue: 'Com. Prov. Vicenza' },
-            { value: 'COM-VR', viewValue: 'Com. Prov. Verona' }
+            { value: 'COM-VE', viewValue: 'Com. Venezia' },
+            { value: 'COM-BL', viewValue: 'Com. Belluno' },
+            { value: 'COM-PD', viewValue: 'Com. Padova' },
+            { value: 'COM-RO', viewValue: 'Com. Rovigo' },
+            { value: 'COM-TV', viewValue: 'Com. Treviso' },
+            { value: 'COM-VI', viewValue: 'Com. Vicenza' },
+            { value: 'COM-VR', viewValue: 'Com. Verona' }
       ]
     },
     {
         name: 'Friuli Venezia Giulia',
         bu: [
             { value: 'DIR-FVG', viewValue: 'Dir. Reg. Friuli Venezia Giulia' },
-            { value: 'COM-TS', viewValue: 'Com. Prov. Trieste' },
-            { value: 'COM-GO', viewValue: 'Com. Prov. Gorizia' },
-            { value: 'COM-PN', viewValue: 'Com. Prov. Pordenone' },
-            { value: 'COM-UD', viewValue: 'Com. Prov. Udine' }
+            { value: 'COM-TS', viewValue: 'Com. Trieste' },
+            { value: 'COM-GO', viewValue: 'Com. Gorizia' },
+            { value: 'COM-PN', viewValue: 'Com. Pordenone' },
+            { value: 'COM-UD', viewValue: 'Com. Udine' }
       ]
     },
     {
         name: 'Liguria',
         bu: [
             { value: 'DIR-LIG', viewValue: 'Dir. Reg. Liguria' },
-            { value: 'COM-GE', viewValue: 'Com. Prov. Genova' },
-            { value: 'COM-IM', viewValue: 'Com. Prov. Imperia' },
-            { value: 'COM-SP', viewValue: 'Com. Prov. La Spezia' },
-            { value: 'COM-SV', viewValue: 'Com. Prov. Savona' }
+            { value: 'COM-GE', viewValue: 'Com. Genova' },
+            { value: 'COM-IM', viewValue: 'Com. Imperia' },
+            { value: 'COM-SP', viewValue: 'Com. La Spezia' },
+            { value: 'COM-SV', viewValue: 'Com. Savona' }
       ]
     },
     {
         name: 'Emilia Romagna',
         bu: [
             { value: 'DIR-EMI', viewValue: 'Dir. Reg. Emilia Romagna' },
-            { value: 'COM-BO', viewValue: 'Com. Prov. Bologna' },
-            { value: 'COM-FE', viewValue: 'Com. Prov. Ferrara' },
-            { value: 'COM-FC', viewValue: 'Com. Prov. Forlì-Cesena' },
-            { value: 'COM-MO', viewValue: 'Com. Prov. Modena' },
-            { value: 'COM-PC', viewValue: 'Com. Prov. Piacenza' },
-            { value: 'COM-PR', viewValue: 'Com. Prov. Parma' },
-            { value: 'COM-RA', viewValue: 'Com. Prov. Ravenna' },
-            { value: 'COM-RE', viewValue: 'Com. Prov. Reggio Emilia' },
-            { value: 'COM-RN', viewValue: 'Com. Prov. Rimini' }
+            { value: 'COM-BO', viewValue: 'Com. Bologna' },
+            { value: 'COM-FE', viewValue: 'Com. Ferrara' },
+            { value: 'COM-FC', viewValue: 'Com. Forlì-Cesena' },
+            { value: 'COM-MO', viewValue: 'Com. Modena' },
+            { value: 'COM-PC', viewValue: 'Com. Piacenza' },
+            { value: 'COM-PR', viewValue: 'Com. Parma' },
+            { value: 'COM-RA', viewValue: 'Com. Ravenna' },
+            { value: 'COM-RE', viewValue: 'Com. Reggio Emilia' },
+            { value: 'COM-RN', viewValue: 'Com. Rimini' }
       ]
     },
     {
         name: 'Toscana',
         bu: [
             { value: 'DIR-TOS', viewValue: 'Dir. Reg. Toscana' },
-            { value: 'COM-FI', viewValue: 'Com. Prov. Firenze' },
-            { value: 'COM-AR', viewValue: 'Com. Prov. Arezzo' },
-            { value: 'COM-GR', viewValue: 'Com. Prov. Grosseto' },
-            { value: 'COM-LI', viewValue: 'Com. Prov. Livorno' },
-            { value: 'COM-LU', viewValue: 'Com. Prov. Lucca' },
-            { value: 'COM-MS', viewValue: 'Com. Prov. Massa-Carrara' },
-            { value: 'COM-PI', viewValue: 'Com. Prov. Pisa' },
-            { value: 'COM-PO', viewValue: 'Com. Prov. Prato' },
-            { value: 'COM-PT', viewValue: 'Com. Prov. Pistoia' },
-            { value: 'COM-SI', viewValue: 'Com. Prov. Siena' }            
+            { value: 'COM-FI', viewValue: 'Com. Firenze' },
+            { value: 'COM-AR', viewValue: 'Com. Arezzo' },
+            { value: 'COM-GR', viewValue: 'Com. Grosseto' },
+            { value: 'COM-LI', viewValue: 'Com. Livorno' },
+            { value: 'COM-LU', viewValue: 'Com. Lucca' },
+            { value: 'COM-MS', viewValue: 'Com. Massa-Carrara' },
+            { value: 'COM-PI', viewValue: 'Com. Pisa' },
+            { value: 'COM-PO', viewValue: 'Com. Prato' },
+            { value: 'COM-PT', viewValue: 'Com. Pistoia' },
+            { value: 'COM-SI', viewValue: 'Com. Siena' }            
       ]
     },
     {
         name: 'Marche',
         bu: [
             { value: 'DIR-MAR', viewValue: 'Dir. Reg. Marche' },
-            { value: 'COM-AN', viewValue: 'Com. Prov. Ancona' },
-            { value: 'COM-AP', viewValue: 'Com. Prov. Ascoli Piceno' },
-            { value: 'COM-MC', viewValue: 'Com. Prov. Macerata' },
-            { value: 'COM-PU', viewValue: 'Com. Prov. Pesaro e Urbino' }
+            { value: 'COM-AN', viewValue: 'Com. Ancona' },
+            { value: 'COM-AP', viewValue: 'Com. Ascoli Piceno' },
+            { value: 'COM-MC', viewValue: 'Com. Macerata' },
+            { value: 'COM-PU', viewValue: 'Com. Pesaro e Urbino' }
       ]
     },
     {
         name: 'Umbria',
         bu: [
             { value: 'DIR-UMB', viewValue: 'Dir. Reg. Umbria' },
-            { value: 'COM-PG', viewValue: 'Com. Prov. Perugia' },
-            { value: 'COM-TR', viewValue: 'Com. Prov. Terni' }
+            { value: 'COM-PG', viewValue: 'Com. Perugia' },
+            { value: 'COM-TR', viewValue: 'Com. Terni' }
       ]
     },
     {
         name: 'Lazio',
         bu: [
             { value: 'DIR-LAZ', viewValue: 'Dir. Reg. Lazio' },
-            { value: 'COM-RM', viewValue: 'Com. Prov. Roma' },
-            { value: 'COM-RT', viewValue: 'Com. Prov. Rieti' },
-            { value: 'COM-VT', viewValue: 'Com. Prov. Viterbo' },
-            { value: 'COM-LT', viewValue: 'Com. Prov. Latina' },
-            { value: 'COM-FR', viewValue: 'Com. Prov. Frosinone' }
+            { value: 'COM-RM', viewValue: 'Com. Roma' },
+            { value: 'COM-RT', viewValue: 'Com. Rieti' },
+            { value: 'COM-VT', viewValue: 'Com. Viterbo' },
+            { value: 'COM-LT', viewValue: 'Com. Latina' },
+            { value: 'COM-FR', viewValue: 'Com. Frosinone' }
         ]
     },
     {
         name: 'Abruzzo',
         bu: [
             { value: 'DIR-ABR', viewValue: 'Dir. Reg. Abruzzo' },
-            { value: 'COM-AQ', viewValue: 'Com. Prov. L"Aquila' },
-            { value: 'COM-CH', viewValue: 'Com. Prov. Chieti' },
-            { value: 'COM-PE', viewValue: 'Com. Prov. Pescara' },
-            { value: 'COM-TE', viewValue: 'Com. Prov. Teramo' }
+            { value: 'COM-AQ', viewValue: 'Com. L"Aquila' },
+            { value: 'COM-CH', viewValue: 'Com. Chieti' },
+            { value: 'COM-PE', viewValue: 'Com. Pescara' },
+            { value: 'COM-TE', viewValue: 'Com. Teramo' }
        ]
     },
     {
         name: 'Molise',
         bu: [
             { value: 'DIR-MOL', viewValue: 'Dir. Reg. Molise' },
-            { value: 'COM-CB', viewValue: 'Com. Prov. Campobasso' },
-            { value: 'COM-IS', viewValue: 'Com. Prov. Isernia' }
+            { value: 'COM-CB', viewValue: 'Com. Campobasso' },
+            { value: 'COM-IS', viewValue: 'Com. Isernia' }
        ]
     },
     {
         name: 'Campania',
         bu: [
             { value: 'DIR-CAM', viewValue: 'Dir. Reg. Campania' },
-            { value: 'COM-NA', viewValue: 'Com. Prov. Napoli' },
-            { value: 'COM-CE', viewValue: 'Com. Prov. Caserta' },
-            { value: 'COM-AV', viewValue: 'Com. Prov. Avellino' },
-            { value: 'COM-SA', viewValue: 'Com. Prov. Salerno' },
-            { value: 'COM-BN', viewValue: 'Com. Prov. Benevento' }
+            { value: 'COM-NA', viewValue: 'Com. Napoli' },
+            { value: 'COM-CE', viewValue: 'Com. Caserta' },
+            { value: 'COM-AV', viewValue: 'Com. Avellino' },
+            { value: 'COM-SA', viewValue: 'Com. Salerno' },
+            { value: 'COM-BN', viewValue: 'Com. Benevento' }
         ]
     },
     {
         name: 'Basilicata',
         bu: [
             { value: 'DIR-BAS', viewValue: 'Dir. Reg. Basilicata' },
-            { value: 'COM-PZ', viewValue: 'Com. Prov. Potenza' },
-            { value: 'COM-MT', viewValue: 'Com. Prov. Matera' }
+            { value: 'COM-PZ', viewValue: 'Com. Potenza' },
+            { value: 'COM-MT', viewValue: 'Com. Matera' }
        ]
     },
     {
         name: 'Puglia',
         bu: [
             { value: 'DIR-PUG', viewValue: 'Dir. Reg. Puglia' },
-            { value: 'COM-BA', viewValue: 'Com. Prov. Bari' },
-            { value: 'COM-BR', viewValue: 'Com. Prov. Brindisi' },
-            { value: 'COM-FG', viewValue: 'Com. Prov. Foggia' },
-            { value: 'COM-LE', viewValue: 'Com. Prov. Lecce' },
-            { value: 'COM-TA', viewValue: 'Com. Prov. Taranto' }
+            { value: 'COM-BA', viewValue: 'Com. Bari' },
+            { value: 'COM-BR', viewValue: 'Com. Brindisi' },
+            { value: 'COM-FG', viewValue: 'Com. Foggia' },
+            { value: 'COM-LE', viewValue: 'Com. Lecce' },
+            { value: 'COM-TA', viewValue: 'Com. Taranto' }
         ]
     },
     {
         name: 'Calabria',
         bu: [
             { value: 'DIR-CAL', viewValue: 'Dir. Reg. Calabria' },
-            { value: 'COM-CZ', viewValue: 'Com. Prov. Catanzaro' },
-            { value: 'COM-CS', viewValue: 'Com. Prov. Cosenza' },
-            { value: 'COM-KR', viewValue: 'Com. Prov. Crotone' },
-            { value: 'COM-RC', viewValue: 'Com. Prov. Reggio Calabria' },
-            { value: 'COM-VV', viewValue: 'Com. Prov. Vibo Valentia' }
+            { value: 'COM-CZ', viewValue: 'Com. Catanzaro' },
+            { value: 'COM-CS', viewValue: 'Com. Cosenza' },
+            { value: 'COM-KR', viewValue: 'Com. Crotone' },
+            { value: 'COM-RC', viewValue: 'Com. Reggio Calabria' },
+            { value: 'COM-VV', viewValue: 'Com. Vibo Valentia' }
         ]
     },
     {
         name: 'Sicilia',
         bu: [
             { value: 'DIR-SIC', viewValue: 'Dir. Reg. Sicilia' },
-            { value: 'COM-PA', viewValue: 'Com. Prov. Palermo' },
-            { value: 'COM-AG', viewValue: 'Com. Prov. Agrigento' },
-            { value: 'COM-CL', viewValue: 'Com. Prov. Caltanissetta' },
-            { value: 'COM-CT', viewValue: 'Com. Prov. Catania' },
-            { value: 'COM-EN', viewValue: 'Com. Prov. Enna' },
-            { value: 'COM-ME', viewValue: 'Com. Prov. Messina' },
-            { value: 'COM-RG', viewValue: 'Com. Prov. Ragusa' },
-            { value: 'COM-SR', viewValue: 'Com. Prov. Siracusa' },
-            { value: 'COM-TP', viewValue: 'Com. Prov. Trapani' }
+            { value: 'COM-PA', viewValue: 'Com. Palermo' },
+            { value: 'COM-AG', viewValue: 'Com. Agrigento' },
+            { value: 'COM-CL', viewValue: 'Com. Caltanissetta' },
+            { value: 'COM-CT', viewValue: 'Com. Catania' },
+            { value: 'COM-EN', viewValue: 'Com. Enna' },
+            { value: 'COM-ME', viewValue: 'Com. Messina' },
+            { value: 'COM-RG', viewValue: 'Com. Ragusa' },
+            { value: 'COM-SR', viewValue: 'Com. Siracusa' },
+            { value: 'COM-TP', viewValue: 'Com. Trapani' }
         ]
     },
     {
         name: 'Sardegna',
         bu: [
             { value: 'DIR-SAR', viewValue: 'Dir. Reg. Sardegna' },
-            { value: 'COM-CA', viewValue: 'Com. Prov. Cagliari' },
-            { value: 'COM-NU', viewValue: 'Com. Prov. Nuoro' },
-            { value: 'COM-OR', viewValue: 'Com. Prov. Oristano' },
-            { value: 'COM-SS', viewValue: 'Com. Prov. Sassari' }
+            { value: 'COM-CA', viewValue: 'Com. Cagliari' },
+            { value: 'COM-NU', viewValue: 'Com. Nuoro' },
+            { value: 'COM-OR', viewValue: 'Com. Oristano' },
+            { value: 'COM-SS', viewValue: 'Com. Sassari' }
         ]
     },
 ];
