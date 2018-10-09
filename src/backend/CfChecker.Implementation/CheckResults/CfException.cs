0a6f9ef5795599a5d56491c0f6333ac0d9dabc93
supix <esposito.marce@gmail.com>
2018-10-09 22:21:10 +0200

    License headers fixed all over the project

--- src/backend/CfChecker.Implementation/CheckResults/CfException.cs
+++ src/backend/CfChecker.Implementation/CheckResults/CfException.cs
@@ -17,7 +17,6 @@
 // along with this program.  If not, see http://www.gnu.org/licenses/.
 // </copyright>
 //-----------------------------------------------------------------------
-
 namespace Services.CfChecker.Impl.CheckResults
 {
     internal class CfException : ICfCheckResult
--- src/backend/DomainModel/Services/IGetActiveApplicationById.cs
+++ src/backend/DomainModel/Services/IGetActiveApplicationById.cs
@@ -17,7 +17,6 @@
 // along with this program.  If not, see http://www.gnu.org/licenses/.
 // </copyright>
 //-----------------------------------------------------------------------
-
 namespace DomainModel.Services
 {
     public interface IGetActiveApplicationById
